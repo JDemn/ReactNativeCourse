d829ba78841aa68347be2f9630d87f6cfc2caaa5
JDemn <dsoftwaremancilla@outlook.es>
2022-07-06 22:51:01 -0500

    FAB android and ios styles

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback, Platform } from 'react-native';
 
 //estableces que propiedades puede recibir este componente
 interface Props {
@@ -9,18 +9,38 @@ interface Props {
 }
 export const Fab = (props: Props) => {
 
-    console.log(props.title);
-
-    return (
-        <TouchableOpacity
-            style={[styles.fabLocation, (props.position === 'br') ? styles.right : styles.left]}
-            onPress={props.onPress}
-        >
-            <View style={styles.fab}>
-                <Text style={styles.fabText}>{props.title}</Text>
+    const ios = () => {
+        return (
+            <TouchableOpacity
+                activeOpacity={0.75}
+                style={[styles.fabLocation, (props.position === 'br') ? styles.right : styles.left]}
+                onPress={props.onPress}
+            >
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>{props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+    const android = () => {
+        return (
+            <View
+                style={[styles.fabLocation, (props.position === 'br') ? styles.right : styles.left]}
+            >
+                <TouchableNativeFeedback
+                    onPress={props.onPress}
+                    background={TouchableNativeFeedback.Ripple('#28425B', false, 10, 30)}
+                >
+                    <View style={styles.fab}>
+                        <Text style={styles.fabText}>{props.title}</Text>
+                    </View>
+                </TouchableNativeFeedback>
             </View>
-        </TouchableOpacity>
-    )
+        )
+    }
+
+    console.log(props.title);
+    return (Platform.OS === 'ios')?ios():android(); 
 }
 
 const styles = StyleSheet.create({
